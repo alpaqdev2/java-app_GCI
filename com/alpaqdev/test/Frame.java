ae9f9f53ec5ef67f537c38e7d3338d79a4ba6e71
alpaqdev2 <58635814+alpaqdev2@users.noreply.github.com>
2019-12-07 18:34:27 +0100

    Add files via upload

---
+++ com/alpaqdev/test/Frame.java
@@ -0,0 +1,11 @@
+package com.alpaqdev.test;
+
+import javax.swing.JFrame;
+
+public class Frame extends JFrame{
+	public Frame() {
+		super("Hello World");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+}
